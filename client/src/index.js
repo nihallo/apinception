76b2091b19cece1fae85953806df8d129cc14c3c
linguo <niokma@gmail.com>
2021-05-08 22:02:13 +0800

    still not working

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
