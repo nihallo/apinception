31c515491afa5d2d71b10adc926a722e26c8d9ec
linguo <niokma@gmail.com>
2021-07-18 10:58:33 +0800

    wip

--- server/controllers/apiService.js
+++ server/controllers/apiService.js
@@ -6,4 +6,11 @@ const router = express.Router();
 export const apiService = async (req, res) => {
     const apiStructure = req.body;
     res.status(200).json(apiStructure );
+    //log input
+    //Validate json input format is correct based on api definition
+    //save request data
+    //start processing steps
+    //save end result
+    //return end result
+
 }
